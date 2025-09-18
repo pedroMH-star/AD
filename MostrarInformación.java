08acafe4f45ae2a85958c368a1a769fa09e81f16
pedroMH-star <pedro.martinezherrero@gmail.com>
2025-09-18 13:03:57 +0200

    Create MostrarInformación.java

---
+++ MostrarInformación.java
@@ -0,0 +1,31 @@
+// Uso de la clase File para mostrar información de ficheros y directorios
+import java.io.File;
+import java.io.IOException;
+
+public class ListadoDirectorio {
+    public static void main(String[] args) {
+        String ruta = ".";
+        if (args.length >= 1) {
+            ruta = args;
+        }
+
+        File fich = new File(ruta);
+        if (!fich.exists()) {
+            System.out.println("No existe el fichero o directorio (" + ruta + ").");
+        } else {
+            if (fich.isFile()) {
+            System.out.println(ruta + " es un fichero.");
+            } else {
+                System.out.println(ruta + " es un directorio. Contenidos: ");
+                File[] ficheros = fich.listFiles(); // Ojo, ficheros o directorios
+                for (File f : ficheros) {
+                    String textoDescr = f.isDirectory() ? "/" : "";
+                    if (f.isFile()) {
+                        textoDescr += " ";
+                    }
+                    System.out.println(textoDescr + f.getName());
+                }
+            }
+        }
+    }
+}
